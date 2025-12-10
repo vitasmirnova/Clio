16bd654e32c321a7033564057e4ae3c777a401dd
vitasmirnova <110938634+vitasmirnova@users.noreply.github.com>
2025-12-10 13:15:27 +0100

    Update custom_functions.py

--- custom_functions.py
+++ custom_functions.py
@@ -332,12 +332,11 @@ def client_contribution(RR, revenue_column):
         grouped_data,
         names='Client_short',
         values=revenue_column,
-        hover_data={'Client': True},     # shows full name
+        # hover_data={'Client': True},     # shows full name
         title=f"Top {round(n*100)}% Clients Contribution to Revenue ({practice_area})",
         hole=0.3,
         hover_name="Client",             # full name only
-        hover_data={revenue_column: True},
-        hover_data={'Client_short': False}
+        hover_data={revenue_column: True, 'Client_short': False},
     )
 
     # fig.update_traces(
